7b2fddb67958f196b59bb5f1585d58d8bb3de55e
alefranii <afmormar@gmail.com>
2017-06-22 11:43:35 -0400

    enviado

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,32 +112,76 @@ function validaciones() {
         (primeraMayus(name.value) || soloLetras(name.value)) &&
         !validarCorreo(email.value) &&
         primeraMayus(city.value)
-    ) {
+        ) {
         limpiarForm();
-        alert('Ya esta registrado, revise su correo');
-        ocultarFormulario();
-    }
+    alert('Ya esta registrado, revise su correo');
+    ocultarFormulario();
+}
 }
 
 
 
-/***************** navbar scroll***********************/
+/***************** mapa***********************/
 
-/*function cambioDeNavbar() {
-    var distanciaY = window.pageYOffset || document.documentElement.scrollTop;
-    var navbar = document.getElementById('navbar');
-    var logo = document.getElementById('logo');
-    var botonSignUp = document.getElementById('btn-sign-up');
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 5,
+    center: {lat: -9.1191427, lng: -77.0349046},
+    mapTypeControl: false,
+    zoomControl: false,
+    streetViewControl: false
+});
 
-    if (distanciaY > 100) {
-        navbar.classList.add('navbar-color');
-        logo.setAttribute('src', 'assets/img/logo.png');
-        /*botonSignUp.style.visibility = 'visible';*/
-
-    /*} else {
-        navbar.classList.remove('navbar-color');
-        logo.setAttribute('src', 'assets/img/logo.png');
-       botonSignUp.style.visibility = 'hidden';
-    }
+  function buscar(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+  }
 }
-window.addEventListener('scroll', cambioDeNavbar);*/
+
+document.getElementById('encuentrame').addEventListener("click", buscar);
+var latitud, longitud;
+var funcionExito = function(posicion){
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
+    var miUbicacion = new google.maps.Marker({
+      position: {lat:latitud, lng:longitud},
+      animation: google.maps.Animation.DROP,
+      map: map
+  });
+    map.setZoom(17);
+    map.setCenter({lat:latitud, lng:longitud});
+}
+var funcionError = function (error){
+    alert("Tenemos un problema con econtrar tu ubicación");
+}
+
+var start = document.getElementById('origen');
+var end = document.getElementById('destino');
+
+new google.maps.places.Autocomplete(start);
+new google.maps.places.Autocomplete(end);
+
+var directionsService = new google.maps.DirectionsService;
+var directionsDisplay = new google.maps.DirectionsRenderer;
+
+var calcularRuta = function(directionsService,directionsDisplay){
+    directionsService.route({
+        origin: start.value,
+        destination: end.value,
+        travelMode: 'DRIVING'
+    }, function (response,status){
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        }else{
+            window.alert(" oh No! No encontramos tu ruta");
+        }
+    });
+}
+
+directionsDisplay.setMap(map);
+var trazarRuta = function(){
+    calcularRuta(directionsService,directionsDisplay);
+};
+
+document.getElementById("ruta").addEventListener('click', trazarRuta);
+};
